0bf1b3acf91af48212c0f283482da8d32fa0d390
Anna Brook <anna.brook@sheffield.ac.uk>
2022-02-09 10:34:32 +0000

    updated script to meta analysis using chapter 4

--- Anna_Script_Testing.R
+++ Anna_Script_Testing.R
@@ -1,1 +1,59 @@
 ### Testing out how to create and save scripts in a branch ###
+
+###Testing out meta-analysis functions and script writing ###
+### using chapter 4 of https://bookdown.org/MathiasHarrer/Doing_Meta_Analysis_in_R/pooling-es.html ###
+##bring the data in ##
+library(tidyverse) # needed for 'glimpse'
+library(dmetar) # needed for the dataset to practise on
+library(meta)
+
+
+## change this data to our dataset
+data(ThirdWave)
+glimpse(ThirdWave)
+
+## work through the meta analysis ##
+## metagen for meta analysis of pre-calculated results 
+## label the column where the effect size is stored as TE
+## label the column where the SE of the effect size is stored as seTE
+## label as studlab whatever you want to use as the study label, eg author but could be title or Covidence ID
+## tell the function what to label the effect size as in output, in this case standardised mean diff SMD
+## specify whether fixed or random effects, in this case RE 
+## specify tau squared calculation method, in this case Max Likelihood 
+## specify whether to use Knapp Hartung adjustment, in this case yes
+## give the analysis a title 
+m.gen <- metagen(TE = TE,
+                 seTE = seTE,
+                 studlab = Author,
+                 data = ThirdWave,
+                 sm = "SMD",
+                 fixed = FALSE,
+                 random = TRUE,
+                 method.tau = "REML",
+                 hakn = TRUE,
+                 title = "Third Wave Psychotherapies")
+
+## get the results 
+summary(m.gen)
+
+## This approach can be adapted for our analysis
+## We would not use SMD for most results 
+## We would need to load our data instead of the test data
+## We would need to label the columns appropriately
+## The other methods are appropriate (RE, ML, adjustment) 
+
+## we probably want to save at this point 
+save(m.gen, file = "C:\Users\md1abzz\Documents\SysRev\Synthesis\AggEconPpnHSysRev") # or can we save direct to github?
+
+##access specific bits of data eg pooled effect size
+m.gen$TE.random
+
+##change some of the ways we did the meta analysis to see whether we get diff results for sensitivity analysis
+## eg change tau squared approach to DerSimonian-Laird (which we probably want to do as a sensitivity analysis)
+m.gen_update <- update.meta(m.gen, 
+                            method.tau = "DL")
+
+# Get pooled effect
+m.gen_update$TE.random
+# Get tau^2 estimate
+m.gen_update$tau2
